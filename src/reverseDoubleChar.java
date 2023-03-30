906e4c0683a87358525d44a2af65e3cf1ac9396f
Jess 💙 <jess@Jesss-Air.lan1>
2023-03-30 00:52:36 -0400

    Initial commit

---
+++ src/reverseDoubleChar.java
@@ -0,0 +1,17 @@
+public class reverseDoubleChar {
+    public String reverseDoubleChar(String word) {
+        String result = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+
+            result += word.charAt(i) + "" + "" + word.charAt(i);
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseDoubleChar("The")); // eehhTT
+        System.out.println(reverseDoubleChar("AABB")); // BBBBAAAA
+        System.out.println(reverseDoubleChar("String")); // ggnniirrttSS
+    }
+}
